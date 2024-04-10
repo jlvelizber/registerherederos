104b9cadcc202073bc4026059cf773d74c6bce7c
Jorge Veliz <jorgeconsalvacion@gmail.com>
2024-04-10 17:07:40 -0500

    Agrega validacion si el QR llega vacio

--- src/events/QrReaderEvent.ts
+++ src/events/QrReaderEvent.ts
@@ -3,7 +3,9 @@ import { EVENTS_NAME } from "../constants"
 import KidModel from "../models/Kid.model"
 export const QrReaderEvent = (socket: Socket) => {
     socket.on(`${EVENTS_NAME.QR_READ}`, async (qr: string) => {
+        if (!qr) return;
         const existKid = await KidModel.findByQr(qr)
+        console.log('entra',existKid)
         //  $2a$10$wDg7TgNUfU34ix4.HdPJtuadaEgb8CEtjSASsYjo3qm7StzX1/9Bu
         if (existKid) {
             socket.emit(EVENTS_NAME.QR_EXIST_KID, { ...existKid })
