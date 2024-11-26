b7bc4e5fde8ec392e70db4404e11e2cccbc6edd2
Jorge Veliz <jorgeconsalvacion@gmail.com>
2024-11-26 11:44:15 -0500

    logger

--- app.ts
+++ app.ts
@@ -1,12 +1,15 @@
 import express, { Application } from "express";
-import logger from "pino-http";
+import pino from "pino";
+import pinohttp from "pino-http";
 import dontenv from "dotenv";
 import routes from "./src/routes";
 import cors from "cors";
 import { Server as ServerHttp, createServer } from "http";
 import { Server } from "socket.io";
 import { QrReaderEvent } from "./src/events";
+
 const PORT = process.env.PORT || 3000;
+
 
 const defaultSocketOptions = {
   cors: {
@@ -16,17 +19,17 @@ const defaultSocketOptions = {
 }
 
 export default class App {
-
+  
   app: Application;
   http: ServerHttp;
   io: Server;
-
+  
   constructor() {
     dontenv.config();
     this.app = express();
     this.http = createServer(this.app);
     this.io = new Server(this.http, defaultSocketOptions)
-
+    
     this.#init()
   }
 
@@ -44,7 +47,7 @@ export default class App {
 
   #configRequest() {
     this.app.use(express.json())
-    this.app.use(logger())
+    this.app.use(pinohttp(pino.destination('./logs/server-log.log')))
     this.app.use(express.static("public"))
   }
 
